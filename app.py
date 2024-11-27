0f7b2d582ddb10eeb24213d2bf0afabee8cdebb9
Tieshan-MiT <simkaijun021006@gmail.com>
2024-11-27 22:37:52 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -33,18 +33,18 @@ line_handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+    #signature = request.headers['X-Line-Signature']
 
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
     # handle webhook body
-    try:
-        line_handler.handle(body, signature)
-    except InvalidSignatureError:
-        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
+    #try:
+    #    line_handler.handle(body, signature)
+    #except InvalidSignatureError:
+    #    app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+    #    abort(400)
 
     return 'OK'
 
